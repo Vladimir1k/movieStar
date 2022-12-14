cceac23489a45bb0bee726ceb3ca8b35c844d6b8
Vladimir1k <106768954+Vladimir1k@users.noreply.github.com>
2022-12-14 15:07:03 +0200

    first commit (#1)
    * first commit
    
    * commit 1.1
    
    * commit 1.2

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	r "movie_star/repository"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.NewClient()
+
+	router.Run("localhost:8080")
+
+}
---
+++ repository/postgresql.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type StorageConfig struct {
+	username, password, host, port, database string
+}
+
+func init() {
+	err := godotenv.Load("./repository/.env")
+	if err != nil {
+		fmt.Println("unable to download .env")
+	}
+
+}
+
+func NewClient() {
+	var sc = StorageConfig{
+		os.Getenv("USERNAME"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("HOST"),
+		os.Getenv("PORT"),
+		os.Getenv("DATABASE"),
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", sc.host, sc.port, sc.username, sc.password, sc.database)
+
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		fmt.Printf("Unable to connect to database: %v\n", err)
+		return
+	}
+
+	defer conn.Close(context.Background())
+
+	fmt.Println("DATABASE CONNECT")
+}
